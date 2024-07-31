26e1e40b02dac45d6b14cc3765d4644bca20f8f1
edimova <emd_vgbg@yahoo.com>
2024-07-31 21:50:19 +0300

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import Header from './components/header/Header'
 import './styles.css'
+
+import { Route, Routes } from 'react-router-dom'
+
+import Header from './components/header/Header'
+import Locations from './components/locations/Locations'
+import Register from './components/register/Register'
+import SignIn from './components/sign-in/SignIn'
+import Logout from './components/logout/Logout'
+import Categories from './components/categories/Categories'
+import Home from './components/home/Home'
 
 function App() {
 
-  return (
-    <>
+	return (
+		<div>
+			<Header />
+			<Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/locations" element={<Locations />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/sing-in" element={<SignIn />} />
+                <Route path="/logout" element={<Logout />} />
+            </Routes>
 
-    <Header/>
-
-  
-     {/* <h1>Events Catalog Project</h1> */}
-    </>
-  )
+		</div>
+	)
 }
 
 export default App
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,14 +1,47 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./Header.module.css"
+
 export default function Header() {
     return (
         <header className="header">
-            <nav>
-                <ul>
-                    <li><a href="#home">HOME</a></li>
-                    <li><a href="#categories">CATEGORIES</a></li>
-                    <li><a href="#locations">LOCATIONS</a></li>
-                    <li><a href="#register">REGISTER</a></li>
-                    <li><a href="#sign-in">SIGN IN</a></li>
-                </ul>
+            <nav >
+                <NavLink
+                    to="/"
+                    className={styles.link}
+                >
+                    HOME
+                </NavLink>
+                <NavLink
+                    to="/categories"
+                    className={styles.link}
+                >
+                    CATEGORIES
+                </NavLink>
+                <NavLink
+                    to="/locations"
+                    className={styles.link}
+                >
+                    LOCATIONS
+                </NavLink>
+                <NavLink
+                    to="/register"
+                    className={styles.link}
+                >
+                    REGISTER
+                </NavLink>
+                <NavLink
+                    to="/sing-in"
+                    className={styles.link}
+                >
+                    SIGN IN
+                </NavLink>
+                <NavLink
+                    to="/logout"
+                    className={styles.link}
+                >
+                    LOG OUT
+                </NavLink>
 
             </nav>
         </header>
