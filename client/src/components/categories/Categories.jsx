b6316a3d83a917f74e14d80061be0947fa52660f
edimova <emd_vgbg@yahoo.com>
2024-08-04 22:22:06 +0300

    add categories

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,5 +1,19 @@
+import { useCategories } from "../../hooks/useCategories";
+import Category from "./Category";
+
 export default function Categories() {
+    const [categories, setCategories] = useCategories();
+    console.log("categories")
+    console.log(categories)
+
     return (
-        <h1>Categories</h1>
+     
+            <div className="row">
+            {categories.map((category) => {
+               return <Category key={category._id} categoryID={category._id}/>
+            })}
+   
+        </div>
+
     )
 }
---
+++ client/src/hooks/useCategories.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import categoriesAPI from "../api/categories-api";
+
+export function useCategories(){
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        (async()=>{
+            const result = await categoriesAPI.getAll();
+            setCategories(result)
+        })()
+
+    }, []);
+    
+
+    return [categories,setCategories];
+
+}
+
+export function useGetOneCaregory(categoryID){
+    const [category, setCategory] = useState([]);
+
+    useEffect(()=>{
+        (async ()=>{
+            const result = await categoriesAPI.getOne(categoryID);
+            setCategory(result);
+        })()
+    },[categoryID])
+    
+    return [category, setCategory];
+
+}
