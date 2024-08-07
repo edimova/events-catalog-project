c3fab9bde1ea3104801927d72919724ca38141d5
edimova <emd_vgbg@yahoo.com>
2024-08-07 21:56:41 +0300

    Header active style

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,21 +13,21 @@ export default function Header() {
         <header className="header">
             <nav className={styles.div}>
                 <div className="row">
-                    <NavLink to="/" className={styles.link} >HOME</NavLink>
-                    <NavLink to="/events" className={styles.link}>EVENTS</NavLink>
-                    <NavLink to="/categories" className={styles.link}>CATEGORIES</NavLink>
-                    <NavLink to="/locations" className={styles.link}>LOCATIONS</NavLink>
+                    <NavLink to="/" className={({isActive})=>isActive?(styles.link_active):(styles.link)} >HOME</NavLink>
+                    <NavLink to="/events" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>EVENTS</NavLink>
+                    <NavLink to="/categories" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>CATEGORIES</NavLink>
+                    <NavLink to="/locations" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>LOCATIONS</NavLink>
 
                     {isAuthenticated ?
                         (<div id="user" className={styles.div}>
-                            <NavLink to="/events/create" className={styles.link}>CREATE EVENT</NavLink>                           
-                            <NavLink to="/logout" className={styles.link}>LOG OUT</NavLink>
-                            <div className={styles.link}> UserName: {username}</div>
+                            <NavLink to="/events/create" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>CREATE EVENT</NavLink>                           
+                            <NavLink to="/logout" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>LOG OUT</NavLink>
+                            <div className={styles.username}> UserName: {username}</div>
                         </div>
                         ) :
                         (<div id="guest" className={styles.div}>
-                            <NavLink to="/register" className={styles.link}>REGISTER</NavLink>
-                            <NavLink to="/login" className={styles.link}>LOGIN</NavLink>
+                            <NavLink to="/register" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>REGISTER</NavLink>
+                            <NavLink to="/login" className={({isActive})=>isActive?(styles.link_active):(styles.link)}>LOGIN</NavLink>
                         </div>
                         )
                     }
