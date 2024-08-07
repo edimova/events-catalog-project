8457a07663ded977e226eafe8e08d84c7548708e
edimova <emd_vgbg@yahoo.com>
2024-08-07 18:49:05 +0300

    Fix Login styling

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,89 +1,80 @@
 import { useEffect, useRef, useState } from "react";
 import { useLogin } from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
 
 export default function Login() {
-    const [error, setError] = useState('');
-    const [loginValues, setLoginValues] = useState({
-        email: '',
-        password: ''
-    });
+  const [error, setError] = useState("");
+  const [loginValues, setLoginValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const navigate = useNavigate();
-    const login = useLogin();
+  const navigate = useNavigate();
+  const login = useLogin();
 
-    const useLoginHandler = async (email, password) => {
-        try {
-            await login(email, password);
+  const useLoginHandler = async (email, password) => {
+    try {
+      await login(email, password);
 
-            navigate('/');
-
-        } catch (error) {
-            setError(error.message)
-        }
-
-
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
     }
-    const inputRef = useRef();
+  };
+  const inputRef = useRef();
 
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    useLoginHandler(loginValues.email, loginValues.password);
+  };
 
+  const changeHandler = (e) => {
+    setLoginValues((oldValues) => ({
+      ...oldValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const formSubmitHandler = (e) => {
-        e.preventDefault();
-        useLoginHandler(loginValues.email, loginValues.password);
-    };
+  return (
+    <section className={styles.form}>
+      <div className={styles.form_container}>
+        <form id="login" onSubmit={formSubmitHandler}>
+          <div className={styles.row}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              ref={inputRef}
+              name="email"
+              id="email"
+              value={loginValues.email}
+              onChange={changeHandler}
+            />
+          </div>
 
-    const changeHandler = (e) => {
-        setLoginValues(oldValues => ({
-            ...oldValues,
-            [e.target.name]: e.target.value,
-        }));
-
-    };
-
-    return (
-        <section className="form">
-            <div className="container">
-                <form id="login" onSubmit={formSubmitHandler}>
-                    <div className="row">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            ref={inputRef}
-                            name="email"
-                            id="email"
-                            value={loginValues.email}
-                            onChange={changeHandler}
-                        />
-                    </div>
-
-                    <div className="row" >
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder="******"
-                            value={loginValues.password}
-                            onChange={changeHandler}
-                        />
-                    </div>
-                    {
-                        error ?? (<p>
-                            {error}
-                        </p>)
-
-                    }
-                    <div>
-                        <input type="submit" value="Login" />
-                    </div>
-
-                </form>
-            </div>
-        </section>
-    );
+          <div className={styles.row}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="******"
+              value={loginValues.password}
+              onChange={changeHandler}
+            />
+          </div>
+          {error ?? <p>{error}</p>}
+          <div>
+            <button className="btn" type="submit">
+              LOGIN
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
